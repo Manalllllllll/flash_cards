cb0b0b8d908a1d572985a973645d6c69a7b70511
manalebraheem072@gmail.com <manalebraheem072@gmail.com>
2023-10-08 17:52:39 +0300

    hhhhg

---
+++ game.swift
@@ -0,0 +1,18 @@
+//
+//  game.swift
+//  flash_cards
+//
+//  Created by alhanouf alhamied on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct game: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    game()
+}
