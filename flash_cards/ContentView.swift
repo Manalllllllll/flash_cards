67943e58373b5da998844fda67a458116c293ccd
Manal <manal_almarwani@icloud.com>
2023-10-08 16:21:33 +0300

    Initial Commit

---
+++ flash_cards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  flash_cards
+//
+//  Created by silva on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ flash_cards/flash_cardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  flash_cardsApp.swift
+//  flash_cards
+//
+//  Created by silva on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct flash_cardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
